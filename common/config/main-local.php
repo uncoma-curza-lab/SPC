c3946209fc4685e16190f916360aa5c6edae4bc2
njmdistrisoft <njmdistrisoft@hotmail.com>
2019-02-21 16:56:06 -0300

    Environment variables config

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,18 +3,23 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=aulasyprogramas',
-            'username' => 'curza',
-            'password' => 'curza',
+            'dsn' => getenv('CONNECT_DB' , 'mysql:host=localhost;dbname=midb'),
+            'username' => getenv('DB_USER','usuario'),
+            'password' => getenv('DB_PASSWORD','clave'),
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => self::env('SMTP_HOST'),
+                'username' => self::env('SMTP_USER'),
+                'password' => self::env('SMTP_PASSWORD'),
+                'port' => self::env('SMTP_PORT', 25),
+                'encryption' => self::env('SMTP_ENCRYPTION', null),
+            ],
         ],
+
     ],
 ];
