234ec792cf77f8e2ab888b362eeade9c70618a47
njmdistrisoft <njmdistrisoft@hotmail.com>
2019-02-21 20:57:13 -0300

    Objetivo de programas

---
+++ common/models/search/ProgramaEvaluacionSearch.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace common\models\search;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Programa;
+use frontend\models\Perfil;
+use common\models\PermisosHelpers;
+use common\models\Departamento;
+use common\models\User;
+use common\models\Cargo;
+use common\models\Designacion;
+use common\models\Status;
+
+
+/**
+ * ProgramaSearch represents the model behind the search form of `common\models\Programa`.
+ */
+
+class ProgramaEvaluacionSearch extends Programa
+{
+    public $departamento;
+    public function attributes(){
+        return array_merge(parent::attributes(),['user.username']);
+    }
+
+    public function attributeLabels(){
+        return array_merge(parent::attributeLabels(),['user.username'=>'nombre de usuario']);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'departamento_id', 'status_id', 'asignatura_id', 'year', 'created_by', 'updated_by'], 'integer'],
+            [[ 'fundament', 'objetivo_plan', 'contenido_plan', 'propuesta_met', 'evycond_acreditacion', 'parcial_rec_promo', 'distr_horaria', 'crono_tentativo', 'actv_extracur', 'created_at', 'updated_at'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $esAdmin = PermisosHelpers::requerirMinimoRol('Admin');
+        $perfil = \Yii::$app->user->identity->perfil;
+        // si es director tiene una designación con ese cargo
+        $cargoDirector = Cargo::find()->where(['=','nomenclatura','Director'])->one();
+        $designacion = Designacion::find()->where(['=','perfil_id',$perfil->id])->andWhere(['=','cargo_id',$cargoDirector->id])->one();
+        $depto = null;
+        if($designacion) {
+          $depto = $designacion->departamento_id;
+        }
+
+
+        $query = Programa::find();
+        if(PermisosHelpers::requerirRol("Departamento"))
+          $query->where(['=','departamento_id',$depto]);
+        else if (PermisosHelpers::requerirRol("Adm_academica") ){
+          $statusAdm_academica = Status::find()->where(['=','descripcion','Administración Académica'])->one();
+          $query->where(['=','status_id',$statusAdm_academica->id]);
+        } else if(PermisosHelpers::requerirRol("Sec_academica")) {
+          $statusSec_academica = Status::find()->where(['=','descripcion','Secretaría Académica'])->one();
+          $query->where(['=','status_id',$statusSec_academica->id]);
+        }
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        //$query->joinWith('user');
+        /*$query->andFilterWhere(
+            ['LIKE','user.username',$this->getAttribute('user.username')]
+        );*/
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'departamento_id' => $this->departamento_id,
+            'status_id' => $this->status_id,
+            'asignatura_id' => $this->asignatura_id,
+            'year' => $this->year,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'created_by' => $this->created_by,
+            'updated_by' => $this->updated_by,
+        ]);
+        $query->joinWith(['status']);
+
+        $query->andFilterWhere(['like', 'fundament', $this->fundament])
+        //->andFilterWhere(['like', 'asignatura', $this->asignatura])
+            ->andFilterWhere(['like', 'objetivo_plan', $this->objetivo_plan])
+            ->andFilterWhere(['like', 'contenido_plan', $this->contenido_plan])
+            ->andFilterWhere(['like', 'propuesta_met', $this->propuesta_met])
+            ->andFilterWhere(['like', 'evycond_acreditacion', $this->evycond_acreditacion])
+            ->andFilterWhere(['like', 'parcial_rec_promo', $this->parcial_rec_promo])
+            ->andFilterWhere(['like', 'distr_horaria', $this->distr_horaria])
+            ->andFilterWhere(['like', 'crono_tentativo', $this->crono_tentativo])
+            ->andFilterWhere(['like', 'actv_extracur', $this->actv_extracur]);
+
+        return $dataProvider;
+    }
+}
---
+++ frontend/views/mi-programa/forms/_objetivo-programa.php
@@ -0,0 +1,70 @@
+<?php
+use dosamigos\tinymce\TinyMce;
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+use yii\helpers\Url;
+$mensaje = [ 'onclick'=>"return confirm('No se guardarán los cambios de esta pestaña, ¿desea salir?')"];
+$this->params['items'][] = ['label' => '1. Fundamentación','url' => Url::to(['cargar', 'id' => $model->id]), 'options'=>[ 'onclick'=>"return confirm('No se guardarán los cambios de esta pestaña, ¿desea salir?')"]];
+$this->params['items'][] = ['label' => '2. Objetivo según plan de estudio', 'url' => Url::to(['objetivo-plan', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '2.1 Objetivos del programa'];
+$this->params['items'][] = ['label' => '3. Contenido según plan de estudio', 'url' => Url::to(['contenido-plan', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '4. Contenidos analíticos', 'url' => Url::to(['contenido-analitico', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '5. Bibliografía', 'url' => Url::to(['bibliografia', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '6. Propuesta Metodológica', 'url' => Url::to(['propuesta-metodologica', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '7. Evaluación y cond. de acreditación', 'url' => Url::to(['eval-acred', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '8. Parciales, recuperatorios y promociones', 'url' => Url::to(['parcial-rec-promo', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '9. Distribución horaria', 'url' => Url::to(['dist-horaria', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '10. Cronograma tentativo', 'url' => Url::to(['crono-tentativo', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['items'][] = ['label' => '11. Actividad extracurricular', 'url' => Url::to(['actividad-extracurricular', 'id' => $model->id]), 'options'=> $mensaje];
+$this->params['breadcrumbs'][] = ['label' => '...'];
+$this->params['breadcrumbs'][] = ['label' => "Obj. según el plan de estudio", 'url' => ['objetivo-plan', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Objetivos del programa';
+$porcentaje = $model->calcularPorcentajeCarga();
+?>
+<div class="row">
+  <div class="col-md-2 text-right">
+    <label>Programa completado: </label>
+  </div>
+  <div class="col-md-10 ">
+    <div class="progress">
+      <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?= $porcentaje ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $porcentaje ?>%">
+         <?= $porcentaje ?>%
+      </div>
+    </div>
+  </div>
+</div>
+
+<h3>2.1 Objetivo del programa</h3>
+  <?php $form = ActiveForm::begin([
+    'enableAjaxValidation'      => false,
+    'enableClientValidation'    => false,
+    'validateOnChange'          => true,
+    'validateOnSubmit'          => false,
+    'validateOnBlur'            => false,
+  ]); ?>
+
+    <?= $form->field($model, 'objetivo_programa')->widget(TinyMce::className(), [
+        'options' => ['rows' => 6],
+        'language' => 'es',
+        'clientOptions' => [
+            'plugins' => [
+                "advlist autolink lists link charmap
+                "//print
+                ."preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime  table contextmenu paste"
+            ],
+            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link | fullscreen "
+        ]
+    ])->label('') ?>
+    <br>
+   <div class="row">
+     <div class="col-xs-6 text-left">
+       <?= Html::a('Atrás', ['objetivo-plan', 'id' => $model->id],['onclick'=>"return confirm('No se guardarán los cambios de esta sección, ¿desea salir?')",'class' => 'btn btn-warning']) ?>
+       <?= Html::submitButton('Guardar y salir',['class' => 'btn btn-info' , 'name'=>'submit','value' => 'salir']) ?>
+     </div>
+     <div class="col-xs-6 text-right">
+       <?= Html::submitButton('Seguir', ['class' => 'btn btn-success']) ?>
+     </div>
+   </div>
+   <?php ActiveForm::end(); ?>
