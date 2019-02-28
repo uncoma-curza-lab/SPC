c26a0baadc74c6c7487472ca3adb8e0ae98e1179
njmdistrisoft <njmdistrisoft@hotmail.com>
2019-02-28 17:48:16 -0300

    fix sql

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -134,7 +134,7 @@ class m130524_201442_init extends Migration
         // user
         $this->createTable('{{%user}}', [
             'id' => Schema::TYPE_PK,
-            'username' => Schema::TYPE_STRING . "(255) NOT NULL UNIQUE",
+            'username' => Schema::TYPE_STRING . "(255) NOT NULL ",
             'auth_key' => Schema::TYPE_STRING . "(32) NOT NULL",
             'password_hash' => Schema::TYPE_STRING . "(255) NOT NULL",
             'password_reset_token' => Schema::TYPE_STRING . "(255) NULL",
