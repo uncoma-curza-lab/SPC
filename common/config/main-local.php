c94199aeca325b12e704e834804d51185a2d6013
fkmurphy <njmdistrisoft@hotmail.com>
2020-07-31 17:30:03 -0300

    Video ayuda

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,7 +4,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => getenv('CONNECT_DB')? getenv('CONNECT_DB') : 'mysql:host=localhost;dbname=aulasyprogramas',
+            'dsn' => getenv('CONNECT_DB')? getenv('CONNECT_DB') : 'mysql:host=mysql;dbname=curza',
             'username' => getenv('DB_USER') ? getenv('DB_USER') : 'curza',
             'password' => getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : 'curza' ,
             'charset' => 'utf8',
