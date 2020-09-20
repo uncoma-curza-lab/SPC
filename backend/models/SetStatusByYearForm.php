b11ba7e22bd3c7e523ba305e1d6d1112a370fb05
fkmurphy <njmdistrisoft@hotmail.com>
2020-09-20 16:25:38 -0300

    cambio de status

---
+++ backend/models/SetStatusByYearForm.php
@@ -0,0 +1,65 @@
+<?php
+namespace backend\models;
+
+use Yii;
+
+use yii\db\ActiveRecord;
+use common\models\User;
+//use common\models\Departamento;
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\db\Expression;
+use common\models\Estado;
+use yii\imagine\Image;
+use common\models\ValorHelpers;
+
+    /**
+     * Change password form for current user only
+     */
+class SetStatusByYearForm extends \yii\db\ActiveRecord
+{
+        public $year, $current_status,$set_status;
+
+        /**
+         * @var \common\models\User
+         */
+     
+        /**
+         * Creates a form model given a token.
+         *
+         * @param  string                          $token
+         * @param  array                           $config name-value pairs that will be used to initialize the object properties
+         * @throws \yii\base\InvalidParamException if token is empty or not valid
+         */
+        public function __construct( $config = [])
+        {
+           
+            parent::__construct($config);
+        }
+     
+        /**
+         * @inheritdoc
+         */
+        public function rules()
+        {
+            return [
+                [['year','current_status','set_status'], 'required', 'message' => 'Es necesario este campo'],
+                [['year'], 'integer' ],
+               
+            ];
+        }
+      /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'year' => 'Año',
+
+        ];
+        
+    }
+
+   
+}
