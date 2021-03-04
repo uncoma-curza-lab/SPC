1b23eea575387b7f621c11e82ee6458c9184b2aa
fkmurphy <njmdistrisoft@gmail.com>
2021-03-04 10:52:47 -0300

    ignore

--- backend/config/main-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'I5RcdYUPLdPX9t08RK8IS9VVU4UbbvbR',
-        ],
-    ],
-];
