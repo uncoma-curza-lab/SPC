7cd293fa617204a48d6c6373b65413decfcfdc9e
Julian Murphy <njmdistrisoft@gmail.com>
2022-07-19 00:35:04 -0300

    deprecated var

--- frontend/controllers/MiProgramaController.php
+++ frontend/controllers/MiProgramaController.php
@@ -165,7 +165,7 @@ class MiProgramaController extends Controller
         $programa->scenario = 'carrerap';
         $userId = \Yii::$app->user->identity->id;
         $estadoActual = Status::findOne($programa->status_id);
-        $porcentajeCarga = 60;
+        //$porcentajeCarga = 60; deprecated 19jul2022
         if ($estadoActual->descripcion == "Borrador"){
           if(!$programa->hasMinimumLoadPercentage()) {
             Yii::error("Error al enviar programa con ID: ".$id.", menos del ". Programa::MIN_LOAD_PERCENTAGE ." cargado",'estado-programa');
