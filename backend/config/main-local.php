20746e28d48dd31aadb86d5fb5434c79a8581d27
Julian Murphy <njmdistrisoft@gmail.com>
2022-11-06 20:21:07 -0300

    only dev

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -9,7 +9,7 @@ $config = [
     ],
 ];
 
-if (YII_ENV != 'prod') {
+if (YII_ENV == 'dev') {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
