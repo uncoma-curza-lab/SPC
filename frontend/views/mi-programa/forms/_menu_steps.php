f39d963943b44d03a9bc4c025336d6579464fad9
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-12-05 13:43:13 -0300

    program steps >> moduels types

--- frontend/views/mi-programa/forms/_menu_steps.php
+++ frontend/views/mi-programa/forms/_menu_steps.php
@@ -1,24 +1,23 @@
 <?php
 
 use common\models\Module;
-use common\models\Programa;
 use yii\helpers\Url;
 $mensaje = [ 'onclick'=>"return confirm('No se guardarán los cambios de esta pestaña, ¿desea salir?')"];
 $breads = [
-    Programa::TIME_DISTRIBUTION_STEP => ['label' => 'Distribución horaria', 'url' => Url::to(['dist-horaria', 'id' => $model->id]), 'options'=>[ 'onclick'=>"return confirm('No se guardarán los cambios de esta pestaña, ¿desea salir?')"]],
+    Module::TIME_DISTRIBUTION_TYPE => ['label' => 'Distribución horaria', 'url' => Url::to(['dist-horaria', 'id' => $model->id]), 'options'=>[ 'onclick'=>"return confirm('No se guardarán los cambios de esta pestaña, ¿desea salir?')"]],
     Module::PROFESSORSHIP_TEAM_TYPE => ['label' => 'Equipo de Catedra','url' => Url::to(['equipo-catedra', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::FUNDAMENTALS_STEP => ['label' => 'Fundamentación','url' => Url::to(['fundamentacion', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::PLAN_OBJECTIVE_STEP => ['label' => 'Objetivos según plan de estudio', 'url' => Url::to(['objetivo-plan', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::PROGRAM_OBJECTIVE_STEP => ['label' => 'Objetivos del programa', 'url' => Url::to(['objetivo-programa', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::PLAN_CONTENT_STEP => ['label' => 'Contenidos según plan de estudio', 'url' => Url::to(['contenido-plan', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::ANALYTICAL_CONTENT_STEP => ['label' => 'Contenidos analíticos', 'url' => Url::to(['contenido-analitico', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::BIBLIOGRAPHY_STEP => ['label' => 'Bibliografía', 'url' => Url::to(['bibliografia', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::METHOD_PROPOSAL_STEP => ['label' => 'Propuesta Metodológica', 'url' => Url::to(['propuesta-metodologica', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::EVALUATION_AND_ACCREDITATION_STEP => ['label' => 'Evaluación y cond. de acreditación', 'url' => Url::to(['eval-acred', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::EXAMS_AND_PROMOTION_STEP => ['label' => 'Parciales, recuperatorios y promociones', 'url' => Url::to(['parcial-rec-promo', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::TIMELINE_STEP => ['label' => 'Cronograma tentativo', 'url' => Url::to(['crono-tentativo', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::ACTIVITIES_STEP => ['label' => 'Actividad extracurricular', 'url' => Url::to(['actividad-extracurricular', 'id' => $model->id]), 'options'=> $mensaje],
-    Programa::SIGN_STEP => ['label' => 'Firma','url' => Url::to(['firma', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::FUNDAMENTALS_TYPE => ['label' => 'Fundamentación','url' => Url::to(['fundamentacion', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::PLAN_OBJECTIVE_TYPE => ['label' => 'Objetivos según plan de estudio', 'url' => Url::to(['objetivo-plan', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::PROGRAM_OBJECTIVE_TYPE => ['label' => 'Objetivos del programa', 'url' => Url::to(['objetivo-programa', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::PLAN_CONTENT_TYPE => ['label' => 'Contenidos según plan de estudio', 'url' => Url::to(['contenido-plan', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::ANALYTICAL_CONTENT_TYPE => ['label' => 'Contenidos analíticos', 'url' => Url::to(['contenido-analitico', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::BIBLIOGRAPHY_TYPE => ['label' => 'Bibliografía', 'url' => Url::to(['bibliografia', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::METHOD_PROPOSAL_TYPE => ['label' => 'Propuesta Metodológica', 'url' => Url::to(['propuesta-metodologica', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::EVALUATION_AND_ACCREDITATION_TYPE => ['label' => 'Evaluación y cond. de acreditación', 'url' => Url::to(['eval-acred', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::EXAMS_AND_PROMOTION_TYPE => ['label' => 'Parciales, recuperatorios y promociones', 'url' => Url::to(['parcial-rec-promo', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::TIMELINE_TYPE => ['label' => 'Cronograma tentativo', 'url' => Url::to(['crono-tentativo', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::ACTIVITIES_TYPE => ['label' => 'Actividad extracurricular', 'url' => Url::to(['actividad-extracurricular', 'id' => $model->id]), 'options'=> $mensaje],
+    Module::SIGN_TYPE => ['label' => 'Firma','url' => Url::to(['firma', 'id' => $model->id]), 'options'=> $mensaje],
 ];
 
 foreach ($breads as $key => $step) {
