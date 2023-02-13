6c2129af9ef8f7bdb0049e74c1a9348addd77487
Manuel Lopez Jove <liaimasoft@gmail.com>
2023-02-13 12:10:04 -0300

    create logs

--- api/config/main.php
+++ api/config/main.php
@@ -35,7 +35,16 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        
+         'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+       
         'urlManager' => [
             'enablePrettyUrl' => true,
             'enableStrictParsing' => true,
