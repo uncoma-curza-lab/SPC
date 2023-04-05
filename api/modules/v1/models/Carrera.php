54e9def93f793b01047fb5068da71d792428dcf5
Julian Murphy <njmdistrisoft@gmail.com>
2023-04-05 18:06:54 -0300

    fix

--- api/modules/v1/models/Carrera.php
+++ api/modules/v1/models/Carrera.php
@@ -55,7 +55,7 @@ class Carrera extends ModelsCarrera implements Linkable
             },
             'planes' => function($model){
                 $planes = Plan::find()->where(['carrera_id' => $model->id])
-                                      ->andWhere(['=','parent_id', null])
+                                      ->andWhere(['is','parent_id', null])
                                       ->andWhere(['activo' => true])
                                       ->all();
                 return $planes;
