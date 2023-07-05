20207b6c5966be0963998cbf532ba4d4b9c62461
Julian Murphy <njmdistrisoft@gmail.com>
2023-07-05 19:14:34 -0300

    program model

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -4,14 +4,17 @@ use api\modules\v1\models\Asignatura as AsignaturaApi;
 use api\modules\v1\models\Plan as PlanApi;
 use api\modules\v1\models\Carrera as CarreraApi;
 use api\modules\v1\models\Departamento as DepartamentoApi;
+use api\modules\v1\models\Programa as ProgramaApi;
 use common\models\Asignatura;
 use common\models\Departamento;
 use common\models\Carrera;
 use common\models\Plan;
+use common\models\Programa;
 
 Yii::$container->setDefinitions([
     Asignatura::class => AsignaturaApi::class,
     Plan::class => PlanApi::class,
     Carrera::class => CarreraApi::class,
     Departamento::class => DepartamentoApi::class,
+    Programa::class => ProgramaApi::class,
 ]);
