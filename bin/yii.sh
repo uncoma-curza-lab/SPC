fc7c4789a51848f3c25c50d856e23c6119aa8d61
Julian Murphy <njmdistrisoft@gmail.com>
2024-04-29 21:33:33 +0000

    Update file yii.sh

---
+++ bin/yii.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker compose exec app yii $@
+
+if [ $1 == migrate/create ] 
+then
+  echo "Cambiando Propietarios de ./migrations ..."
+  docker compose exec app /bin/sh -c "chown \$PHP_USER_ID:\$PHP_USER_ID ./migrations -R" &&\
+   echo "Propietario modificado"
+fi
