272b565c3ce3eb5fcca3820ddafb12decab5e82a
Julian Murphy <njmdistrisoft@gmail.com>
2024-05-08 08:46:00 -0300

    views

--- console/migrations/m240508_023356_full_courses_view.php
+++ console/migrations/m240508_023356_full_courses_view.php
@@ -13,31 +13,36 @@ class m240508_023356_full_courses_view extends Migration
     public function safeUp()
     {
         $this->execute("
-            CREATE TABLE full_courses_view AS
-            SELECT
-                c.id,
-                c.orden,
-                c.nomenclatura,
-                c.curso,
-                c.cuatrimestre,
-                c.carga_horaria_sem,
-                c.carga_horaria_cuatr,
-                c.requisitos,
-                p.planordenanza,
-                d.nom AS department_name,
-                d.id AS department_id,
-                department_auditor.id AS department_auditor_id,
-                department_auditor.nom AS department_auditor_name
-            FROM
-                asignatura c
-            JOIN plan p ON
-                p.id = c.plan_id
-            JOIN carrera career ON career.id = p.carrera_id
-            LEFT JOIN asignatura c_parent ON
-                c_parent.id = c.parent_id
-            JOIN departamento department_auditor ON
-                department_auditor.id = c.departamento_id
-            JOIN departamento d ON d.id = career.departamento_id;
+
+            CREATE PROCEDURE refresh_full_courses_materialized_view()
+            BEGIN
+                TRUNCATE TABLE full_courses_view;
+                CREATE TABLE full_courses_view AS
+                SELECT
+                    c.id,
+                    c.orden,
+                    c.nomenclatura,
+                    c.curso,
+                    c.cuatrimestre,
+                    c.carga_horaria_sem,
+                    c.carga_horaria_cuatr,
+                    c.requisitos,
+                    p.planordenanza,
+                    d.nom AS department_name,
+                    d.id AS department_id,
+                    department_auditor.id AS department_auditor_id,
+                    department_auditor.nom AS department_auditor_name
+                FROM
+                    asignatura c
+                JOIN plan p ON
+                    p.id = c.plan_id
+                JOIN carrera career ON career.id = p.carrera_id
+                LEFT JOIN asignatura c_parent ON
+                    c_parent.id = c.parent_id
+                JOIN departamento department_auditor ON
+                    department_auditor.id = c.departamento_id
+                JOIN departamento d ON d.id = career.departamento_id;
+            END
         ");
 
 
@@ -50,6 +55,7 @@ class m240508_023356_full_courses_view extends Migration
     {
         $this->execute("
             TRUNCATE TABLE full_courses_view;
+            DROP PROCEDURE IF EXISTS refresh_full_courses_materialized_view;
          ");
     }
 
